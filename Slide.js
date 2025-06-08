a288f8c8fee41263aaae9f5b1e62189419e6591b
Subodha Sathyangi <subosathya2003@gmail.ccom>
2025-06-09 01:06:57 +0530

    Initial commit of all local project files

---
+++ Slide.js
@@ -0,0 +1,24 @@
+// Slide functionality
+let currentIndex = 0;
+const slides = document.querySelector('.slides');
+const totalSlides = slides.children.length;
+const nextButton = document.querySelector('.next-button');
+const prevButton = document.querySelector('.prev-button');
+
+
+// Change slide every 3 seconds
+setInterval(showNextSlide, 3000);
+function showNextSlide() {
+    currentIndex = (currentIndex + 1) % totalSlides;
+    const offset = -currentIndex * 100;
+    slides.style.transform = `translateX(${offset}%)`;
+  }
+  
+function showPrevSlide() {
+  currentIndex = (currentIndex - 1 + totalSlides) % totalSlides; // Handle negative index
+  const offset = -currentIndex * 100;
+  slides.style.transform = `translateX(${offset}%)`;
+}
+
+nextButton.addEventListener('click', showNextSlide);
+prevButton.addEventListener('click', showPrevSlide);
